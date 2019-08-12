604bb550db190e6c7b1f59ed03d103282b0a3fbc
Michael Asemota <masemot2@my.centennialcollege.ca>
2019-08-12 12:52:57 -0400

    Created Character and item classes

---
+++ COMP123-S2019-FinalTestB/Objects/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Student Name: Michael Asemota
+ * Student ID: 301052117
+ * Description: This is the character class and the data container for the forms
+ */
+
+namespace COMP123_S2019_FinalTestB.Objects
+
+{
+    class Character
+    {
+        // Character Abilities
+        public string Strength { get; set; }
+        public string Dexterity { get; set; }
+        public string Constitution { get; set; }
+        public string Intelligence { get; set; }
+        public string Wisdom { get; set; }
+        public string Charisma { get; set; }
+
+        // Secondary Abilities
+        public int ArmourClass { get; set; }
+        public int HitPoints { get; set; }
+
+        //Character Class
+        public string CharacterClass { get; set; }
+        public int Level { get; set; }
+
+        //Equipment
+        List<Item> Inventory;
+
+        //Constructor
+        Character()
+        {
+            this.Inventory = new List<Item>();
+        }
+    }
+}
