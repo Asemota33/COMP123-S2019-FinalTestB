c26270b7567373c04a5c4158edaa86ac11973e02
Michael Asemota <masemot2@my.centennialcollege.ca>
2019-08-12 13:17:39 -0400

    Added Master Form and Character Generation Form

---
+++ COMP123-S2019-FinalTestB/Objects/CharacterClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Student Name: Michael Asemota
+ * Student ID: 301052117
+ * Description: This is an enumeration for the Character Class 
+ */
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    public enum CharacterClass
+    {
+        FIGHTER,
+        ROGUE,
+        WIZARD,
+        CLEREIC,
+        RANGER,
+        MONK,
+        BARBARIAN,
+        PALADIN,
+        BARD,
+        WARLOCK,
+        SORCEROR,
+        DRUID
+    }
+}
